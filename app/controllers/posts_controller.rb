d47c8be7a9f253bbe52e9c4c06fdbae5710887ca
daniel321c <321dchen@gmail.com>
2015-07-15 14:18:50 +0800

    commit to heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,61 @@
 class PostsController < ApplicationController
 	def index
-		@content_first = "This is some sample text for our awesome new ruby blog";
-		@content_second = "This is the second content";
+		@posts = Post.all
 
+	end
+
+
+	def show
+
+		@post = Post.find(params[:id])
 
 	end
 
 	def new
+		@post = Post.new
+		@category = Category.all
 
 	end
 
 	def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path, :notice => "Your post has been saved."
+		else
+			render "new"
+		end
 
 	end
 
 	def edit
 
+		@post = Post.find(params[:id])
+
 	end
 
 	def update
 
+		@post = Post.find(params[:id])
+		if @post.update_attributes(params[:post])
+			redirect_to post_path, :notice =>"Your Post has been updated"
+		else
+			render "Edit"
+		end
+
+
 	end
 
-	def show 
-
-	end
 
 	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "your post is deleted."
 
 	end
+
+	private
+ 	def post_params
+  	params.require(:post).permit(:title, :body)
+ 	end
+
 end
