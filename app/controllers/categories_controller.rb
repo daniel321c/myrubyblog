6b6177818926cfc91d9e6bb4231e8bb80fe63c22
daniel321c <321dchen@gmail.com>
2015-07-30 13:44:01 +0800

    categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,20 @@
 class CategoriesController < ApplicationController
+  # before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  # GET /categories
+  # GET /categories.json
+  
   def index
-  end
+    @categories = Category.all
 
-  def edit
-  end
-
-  def new
   end
 
   def show
+    @category = Category.find(params[:id])
+    @title = @category.name
+    @posts = @category.posts
+
   end
+
+ 
 end
